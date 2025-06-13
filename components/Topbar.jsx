d27ebbbf6386bd162eba89bd11c48299460584cf
Shoyu <shuyong.yang@daolabs.com>
2025-06-13 22:45:57 +0700

    add ca

--- components/Topbar.jsx
+++ components/Topbar.jsx
@@ -7,7 +7,7 @@ export default function Topbar() {
   return (
     <div className="flex justify-end items-center px-6 mb-10 space-x-4">
       <a
-        href="https://pump.fun/coin/"
+        href="https://pump.fun/coin/2pHPoTfkRjhxqo5JDzEyqRTftQivJHCiUiT5uGp2pump"
         target="_blank"
         rel="noopener noreferrer"
         className="bg-yellow-500 hover:bg-yellow-600 text-black font-semibold px-4 py-2 rounded"
