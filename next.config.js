6dcdb1a24f3da6051922591c8b684cfb61d4de44
Shoyu <shuyong.yang@daolabs.com>
2025-06-13 21:49:23 +0700

    final fix

--- next.config.js
+++ next.config.js
@@ -13,11 +13,5 @@ module.exports = {
       },
     ];
   },
-  webpack: (config) => {
-    config.externals = config.externals || {};
-    config.externals['@solana/web3.js'] = 'commonjs @solana/web3.js';
-    config.externals['@solana/spl-token'] = 'commonjs @solana/spl-token';
-    return config;
-  },
 };
 
